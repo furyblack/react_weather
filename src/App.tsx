1996bd20f6d24ee9abdf5a03367f96e9a9ef0970
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2024-10-17 22:27:28 +0500

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [city, setCity] = useState<string>('');
+    const [error, setError] = useState<string | null>(null); // Добавляем стейт для ошибки
+
+    const fetchWeather = () => {
+        const apiKey = '1ce2c8f1225f14bf33b893989c1548cf';
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+            .then(response => response.json())
+            .then(json => {
+                if (json.cod === "404") {
+                    setError('City not found');
+                } else {
+                    console.log(json);
+                    setError(null);
+                }
+            })
+            .catch(error => {
+                console.error('Ошибка:', error);
+                setError('An error occurred');
+            });
+    };
+
+    return (
+        <div className="App">
+            <h1>Weather App</h1>
+            <input
+                type="text"
+                value={city}
+                onChange={(e) => setCity(e.currentTarget.value)}
+            />
+            <button onClick={fetchWeather}>Get weather</button>
+            {error && <p style={{ color: 'red' }}>{error}</p>} {/* Отображаем ошибку */}
+        </div>
+    );
 }
 
 export default App;
